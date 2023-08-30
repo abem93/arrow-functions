dc9840a984eddd2d733acf101fe6447851eb1b6e
Abraham Mejia <aimejia4@gmail.com>
2023-08-30 02:11:03 -0500

    Assignment completed

---
+++ script.js
@@ -0,0 +1,18 @@
+// Script a minimum of three arrow functions:
+// A function that accepts two arguments and returns their product.
+
+let product = (a,b) => a * b;
+console.log(product(4,5))   //20
+
+// A function that takes in a string and returns it in uppercase.
+
+let uppercase = s => s.toUpperCase();
+
+console.log(uppercase('hello'));
+
+// A function with a single parameter, calculating the square of the provided number.
+
+let numSquared = t => t*t;
+
+console.log(numSquared(4));
+
